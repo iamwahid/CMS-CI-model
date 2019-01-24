8ab4d239f4a111e5e8eaf0656f775eafe880fcb1
Wahid Niam <iam.wahidn@gmail.com>
2019-01-24 17:58:48 +0700

    search pagination

--- ci/application/controllers/Post.php
+++ ci/application/controllers/Post.php
@@ -1,20 +1,35 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Post extends CI_Controller {
     public function index() {
         $this->load->model('mpost');
         $data = $this->mpost->get();
-        
+        // $data = $this->page();
+        echo "<ul>";
+        foreach ($data as $value) {
+            $title = $value->title;
+            $content = $value->content;
+            $slug = $value->slug;
+            echo "<li>";
+            echo "<b>$title</b><br>$content"." <a href='single/$slug'>here</a>";
+            echo "</li>";
+        }
+        echo "</ul>";
+
+        // echo $this->pagination->create_links();
         
     }
 
     public function add() {
+        $judul = "City in Indonesia";
         $data = [
             "id" => "",
             "author" => 1,
-            "title" => "New Post",
-            "content" => "New Post",
+            "title" => $judul,
+            "content" => "PonorogoxMadiun, Jakarta, Bandung",
             "status" => 0,
-            "slug" => "New-Post",
+            "slug" => strtolower(str_replace(' ', '-', $judul)),
             "parent" => 0,
             "post_type" => "post",
             "post_created" => date('Y-m-d h:m:s:ss'),
@@ -35,6 +50,36 @@ class Post extends CI_Controller {
         echo "<p>$content</p>";
 
     }
+
+    public function cari($keyword) {
+        $this->load->model('mpost');
+        $data = $this->mpost->search($keyword, 'content');
+        foreach ($data as $value) {
+            $judul = $value->title;
+            $isi = $value->content;
+            echo $judul." : $isi <br>";
+        }
+    }
+
+    public function page() {
+        $this->load->model('mpost');
+        $total = $this->mpost->countTotal();
+        $this->load->library('pagination');
+        $config['base_url'] =  base_url().'index.php/post/page/';
+        $config['total_rows'] = $total;
+        $config['per_page'] = 5;
+        $from = $this->uri->segment(3);
+        $this->pagination->initialize($config);
+        $post = $this->mpost->show($config['per_page'],$from);
+        foreach ($post as $value) {
+            echo $value->title."<br>";
+            echo $value->id."<br>";
+        }
+
+        echo "<br>";
+        echo $this->pagination->create_links();
+    }
+
 }
 
 
--- ci/application/models/Mpost.php
+++ ci/application/models/Mpost.php
@@ -5,5 +5,16 @@ class Mpost extends CMS_Model {
         parent::__construct($table);
     }
 
+    public function search($keyword, $column = 'title') {
+        $this->db->select('*');
+        $this->db->from($this->table);
+        $this->db->like($column, $keyword);
+        return $this->db->get()->result();
+    }
+
+    function show($num, $offs) {
+        return $query  = $this->db->get($this->table, $num, $offs)->result();
+    }
+
 }
 ?>
